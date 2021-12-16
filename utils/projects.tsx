9816c2c5c54fe3b78a18ccb5bdd265f3fb17eaa7
bt3gl <1130416+bt3gl@users.noreply.github.com>
2021-12-16 04:45:58 +0000

    Update projects.tsx

--- utils/projects.tsx
+++ utils/projects.tsx
@@ -22,7 +22,7 @@ const loot: Project = {
   opensea: {
     url: "https://opensea.io/thefilmmakerdao.eth"
   },
-  image: "https://gateway.pinata.cloud/ipfs/QmepbViJRBN2wkWzPVAYvvpoFSPSyjoGxXoSHNzuwTFkPh"
+  image: "./card.svg"
 };
 
 export {
