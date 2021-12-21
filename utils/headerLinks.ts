d1db5a1a60709fc85e2555064168b7e0c8f1fc40
bt3gl <1130416+bt3gl@users.noreply.github.com>
2021-12-21 03:15:42 +0000

    Update headerLinks.ts

--- utils/headerLinks.ts
+++ utils/headerLinks.ts
@@ -3,7 +3,7 @@ export const headerLinks: Array<Links> = [
 
     {
         name: "Formation Proposal",
-        path: "https://snapshot.org/#/thefilmmakerdao.eth/proposal/0xe7d4a39d2f89de236cf11b63b99dcb8f4a6121242cddecd8c81ef5d7a6ecff6a",
+        path: "https://snapshot.org/#/thefilmmakerdao.eth/proposal/0xeae530fea4e6aa7522497ddcf35bdce50818e35d0f6fb11e88925bd61cec181f",
     },
     {
         name: "FAQ",
