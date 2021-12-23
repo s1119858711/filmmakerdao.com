7e369326f388db5b807ac895ef7c8c5def9581cb
bt3gl <1130416+bt3gl@users.noreply.github.com>
2021-12-23 02:07:24 +0000

    Update headerLinks.ts

--- utils/headerLinks.ts
+++ utils/headerLinks.ts
@@ -5,14 +5,15 @@ export const headerLinks: Array<Links> = [
         name: "Formation Proposal",
         path: "https://snapshot.org/#/thefilmmakerdao.eth/proposal/0xeae530fea4e6aa7522497ddcf35bdce50818e35d0f6fb11e88925bd61cec181f",
     },
-    {
-        name: "FAQ",
-        path: "/faq"
-    },
+
     
         {
         name: "Newsletter",
-        path: "https://story.mirror.xyz/"
+        path: "https://story.mirror.xyz/gbwXf_IOm4BkUNmWYWz5M-47Te8ELuQngClR8iHFxDU"
+    },
+        {
+        name: "FAQ",
+        path: "/faq"
     },
 
     
